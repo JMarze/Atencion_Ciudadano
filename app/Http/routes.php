93942130f6e205e38a0e397e1fa8a4f2cd6e5a93
JMarze <arze.jesus@gmail.com>
2017-02-09 18:00:36 -0400

    Rutas Autenticación para AdminLTE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+    Route::get('/admin', 'HomeController@index');
+});
